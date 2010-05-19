5bc94f5d3c0fc8640ec9d72cfd5234dfc2566079
Matteo Redaelli <matteo.redaelli@libero.it>
2010-05-19 21:59:15 +0200

    added stats for web crawlers

--- utils/chart-rrd.sh
+++ utils/chart-rrd.sh
@@ -10,52 +10,52 @@ rrdpath=../priv/data
 rrdfile=email_clients.rrd
 outdir=../priv/www
 
-for start in -1hours -1days -7days -31days
+for start in -15m -1hours -1days -7days -31days
 do
     rrd="ebot_amqp"
 
 #  --lower-limit 1
     /usr/bin/rrdtool graph $outdir/${rrd}${start}.png -a PNG   \
         --start $start --end now --step $step              \
-        --title "${rrd} ~ ${start}"       \
+        --title "${rrd} ${start}"       \
         --vertical-label "Totals" --units-length 6 \
         --width $width --height $height --units-exponent 0    \
-        DEF:my0=${rrdpath}/${rrd}.rrd:queue_new_0:AVERAGE              \
-        DEF:my1=${rrdpath}/${rrd}.rrd:queue_new_1:AVERAGE              \
-	DEF:my2=${rrdpath}/${rrd}.rrd:queue_new_2:AVERAGE              \
-	DEF:my3=${rrdpath}/${rrd}.rrd:queue_new_3:AVERAGE              \
-	DEF:my4=${rrdpath}/${rrd}.rrd:queue_new_4:AVERAGE              \
-        LINE0:my0#FF0000:"queue0"	\
-        LINE1:my1#0000FF:"queue1"	\
-	LINE2:my2#00FFFF:"queue2"	\
-	LINE3:my3#00FF00:"queue3"	\
-	LINE4:my4#00FF00:"queue4" 
+        DEF:my1=${rrdpath}/${rrd}.rrd:queue_new_0:AVERAGE              \
+        DEF:my2=${rrdpath}/${rrd}.rrd:queue_new_1:AVERAGE              \
+	DEF:my3=${rrdpath}/${rrd}.rrd:queue_new_2:AVERAGE              \
+	DEF:my4=${rrdpath}/${rrd}.rrd:queue_new_3:AVERAGE              \
+	DEF:my5=${rrdpath}/${rrd}.rrd:queue_new_4:AVERAGE              \
+        LINE1:my1#FF0000:"queue0"	\
+        LINE2:my2#0000FF:"queue1"	\
+	LINE3:my3#00FFFF:"queue2"	\
+	LINE4:my4#00FF00:"queue3"	\
+	LINE5:my5#FFFF00:"queue4" 
 
     rrd="ebot_db"
 
     /usr/bin/rrdtool graph $outdir/${rrd}${start}.png -a PNG   \
         --start $start --end now --step $step              \
-        --title "${rrd} ~ ${start}"       \
+        --title "${rrd} ${start}"       \
         --vertical-label "Totals" --units-length 6 \
         --width $width --height $height --units-exponent 0    \
-        DEF:my0=${rrdpath}/${rrd}.rrd:disk_size:AVERAGE              \
-        DEF:my1=${rrdpath}/${rrd}.rrd:doc_count:AVERAGE              \
-        LINE1:my0#FF0000:"Disk size"	\
-        LINE2:my1#0000FF:"Doc count"
+        DEF:my1=${rrdpath}/${rrd}.rrd:disk_size:AVERAGE              \
+        DEF:my2=${rrdpath}/${rrd}.rrd:doc_count:AVERAGE              \
+        LINE1:my1#FF0000:"Disk size"	\
+        LINE2:my2#0000FF:"Doc count"
 
     rrd="ebot_web"
 
 #  --lower-limit 1
     /usr/bin/rrdtool graph $outdir/${rrd}${start}.png -a PNG   \
         --start $start --end now --step $step              \
-        --title "${rrd} ~ ${start}"       \
+        --title "${rrd} ${start}"       \
         --vertical-label "Totals" --units-length 6 \
         --width $width --height $height --units-exponent 0    \
-        DEF:my0=${rrdpath}/${rrd}.rrd:crawlers_0:AVERAGE              \
-        DEF:my1=${rrdpath}/${rrd}.rrd:crawlers_1:AVERAGE              \
-	DEF:my2=${rrdpath}/${rrd}.rrd:crawlers_2:AVERAGE              \
-        LINE0:my0#FF0000:"crawlers_0"	\
-        LINE1:my1#0000FF:"crawlers_1"	\
-	LINE2:my2#00FFFF:"crawlers_2"      
+        DEF:my1=${rrdpath}/${rrd}.rrd:crawlers_0:AVERAGE              \
+        DEF:my2=${rrdpath}/${rrd}.rrd:crawlers_1:AVERAGE              \
+	DEF:my3=${rrdpath}/${rrd}.rrd:crawlers_2:AVERAGE              \
+        LINE1:my1#FF0000:"crawlers_0"	\
+        LINE2:my2#0000FF:"crawlers_1"	\
+	LINE3:my3#00FF00:"crawlers_2"      
 done
 
