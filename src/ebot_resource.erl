17cc12ab6cd019c60fc16fbd37287a53d126189c
Matteo Redaelli <matteo.redaelli@libero.it>
2010-05-22 11:00:48 +0200

    new urls can be submetted by GET http request (in the future POST/PUT)

--- src/ebot_resource.erl
+++ src/ebot_resource.erl
@@ -5,7 +5,10 @@
 -module(ebot_resource).
 -export([
 	 init/1,
+	 allowed_methods/2,
+	 content_types_accepted/2, 
 	 content_types_provided/2, 
+	 to_json/2,
 	 to_text/2
 	]).
 
@@ -13,46 +16,55 @@
 
 init([]) -> {ok, undefined}.
 
+allowed_methods(ReqData, State) ->
+    {['GET', 'POST'], ReqData, State}.
+
+content_types_accepted(ReqData, State) ->
+    {[{"application/json", from_json}], ReqData, State}.
+
 content_types_provided(ReqData, State) ->
-   {[{"text/plain",to_text}], ReqData, State}.
+   {[{"text/plain",to_text},{"application/json", to_json}], ReqData, State}.
+
+to_json(ReqData, State) ->
+    {json_body(wrq:req_qs(ReqData)), ReqData, State}.
 
 to_text(ReqData, State) ->
     {ok, Command} = dict:find(command, wrq:path_info(ReqData)),
     Tokens = wrq:path_tokens(ReqData),
-    case Command of
-   	"crawlers" ->
-    	    Result = command_crawlers(Tokens);
-    	"ping" ->
-    	    Result = "pong";
-    	"stats" ->
-    	    Result = command_stats(Tokens);
-    	_ ->
-    	    Result = "Unknown command"
-    end,
+    Method = wrq:method(ReqData),
+    Result = command(Method, Command, Tokens, ReqData),
     {Result, ReqData, State}.
 
+%% INTERNAL FUNCTIONS
 
-command_stats([]) ->
-    "Unknow option for stats";
-command_stats([Command|_Tokens]) ->
+json_body(QS) -> mochijson:encode({struct, QS}).
+
+command(_Method, "ping", _Tokens, _ReqData) ->
+    "pong";
+
+%% urls must be at least
+%% /.+/.+ 
+
+command(Method, Command, [], _ReqData) ->
+    io_lib:format("Unknow option: method=~ts, command=~ts", [Method, Command]);
+
+command(_Method, "crawlers", [Command|_Tokens], ReqData) ->
     case Command of
-	"show" ->
-    	    Result = ebot_stats:statistics();
-    	"update_rrd" ->
-	    Path = filename:join(
-                         [filename:dirname(code:which(?MODULE)),
-                          "..", "priv", "data"]),
-    	    Result = ebot_stats:update_rrd_statistics(Path);
-    	_ ->
-    	    Result = command_stats([])
-    end,
-    Result.
-
-
-command_crawlers([]) ->
-    "Unknow option for crawlers";
-command_crawlers([Command|_Tokens]) ->
-    case Command of
+	"add_url" ->
+%	    case Method of
+%		'POST' ->
+%		    Result = "ok";
+%		_Else ->
+%		    Result = "Only POST request is supported"
+%	    end;
+	    Url = wrq:get_qs_value("url",ReqData),
+	    case is_list(Url) of
+		true ->
+		    ebot_memcache:add_new_url( list_to_binary(Url) ),
+		    Result = "Added url: " ++ Url;
+		false ->
+		    Result = "Invalid/Missing parameter url"
+	    end;
     	"check_recover" ->
 	    Before = ebot_web:show_crawlers_list(),
     	    After = ebot_web:check_recover_crawlers(),
@@ -74,9 +86,22 @@ command_crawlers([Command|_Tokens]) ->
 	    Result = "Stopping " ++ 
 		integer_to_list(Tot1) ++
 		" crawlers";
-    	_ ->
-    	    Result = command_stats([])
+    	Else ->
+    	    Result = Else
+    end,
+    Result;
+
+command(_Method, "stats", [Command|_Tokens], _ReqData) ->
+    case Command of
+	"show" ->
+    	    Result = ebot_stats:statistics();
+    	"update_rrd" ->
+	    Path = filename:join(
+                         [filename:dirname(code:which(?MODULE)),
+                          "..", "priv", "data"]),
+    	    Result = ebot_stats:update_rrd_statistics(Path);
+    	Else ->
+    	    Result = Else
     end,
     Result.
-
 
